beaf7e04fe28e1446a264a0022df748b8d4b4557
Laila Alba <z1001@yandex.ru>
2025-05-15 17:46:35 +0300

    добавляет рендер кардс

--- js/data.js
+++ js/data.js
@@ -1,30 +1,49 @@
-import {getRandomInteger, getComments, createPhoto} from './util.js';
+import {
+  MIN_COMMENTS,
+  MAX_COMMENTS,
+  MIN_COMMENTS_ID,
+  MAX_COMMENTS_ID,
+  MIN_AVATAR,
+  MAX_AVATAR,
+  MESSAGES,
+  NAMES,
+  MIN_LIKES,
+  MAX_LIKES,
+  DESCRIPTIONS,
+  PHOTOS_COUNT
+} from './constants.js';
+import {getRandomInteger} from './util.js';
 
-const PHOTOS_COUNT = 25;
-const MIN_LIKES = 15;
-const MAX_LIKES = 200;
-const MIN_COMMENTS = 0;
-const MAX_COMMENTS = 30;
-const MAX_COMMENTS_ID = 10000;
-const MIN_COMMENTS_ID = 100;
-const MIN_AVATAR = 1;
-const MAX_AVATAR = 6;
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.!',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.'
-];
+const getComments = () => {
+  const countComments = getRandomInteger(MIN_COMMENTS, MAX_COMMENTS);
+  const comments = [];
 
-const NAMES = [
-  'Шерочка',
-  'Машерочка',
-  'Проверочка'
-];
+  for(let i = 1; i <= countComments; i++){
+    comments.push({
+      id: getRandomInteger(MIN_COMMENTS_ID, MAX_COMMENTS_ID),
+      avatar: `img/avatar-${getRandomInteger(MIN_AVATAR, MAX_AVATAR)}.svg`,
+      message: MESSAGES[getRandomInteger(0,MESSAGES.length - 1)],
+      names: NAMES[getRandomInteger(0, NAMES.length - 1)]
+    });
+  }
+  return comments;
+};
 
-const DESCRIPTIONS = [
-  'Пляж',
-  'Клубника',
-  'Концерт'
-];
+const createPhoto = (index) => ({
+  id: index,
+  url: `./photos/${index}.jpg`,
+  likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+  description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
+  comments: getComments()
+});
 
-export {createPhoto};
+
+export const createPhotos = () => {
+
+  const photos = [];
+
+  for (let i = 1; i <= PHOTOS_COUNT; i++) {
+    photos.push(createPhoto(i));
+  }
+  return photos;
+};
